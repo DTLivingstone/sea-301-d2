1e624e48bb1e8daa3d57df7b7937f78fc3c310ef
Lacey Vu <laceylin2010@gmail.com>
2016-01-05 16:30:27 -0800

    everything up to date

--- class-02-jquery-and-events/pair-assignment/david-lacey/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/david-lacey/scripts/articleView.js
@@ -50,7 +50,7 @@ articleView.handleAuthorFilter = function() {
 
 articleView.handleCategoryFilter = function() {
 
-  
+
   // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
